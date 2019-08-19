ba91d6fde7cca10a49a7dfa62a4ffe1280e363d7
Erick Tepale <e.tepale@gmail.com>
2019-08-19 17:01:08 -0400

    Initial commit

---
+++ src/main/java/com/guide/guide7/Properties/StorageProperties.java
@@ -0,0 +1,16 @@
+package com.guide.guide7.Properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private String location = "upload-dir";
+
+    public String getLocation(){
+        return location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/guide/guide7/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package com.guide.guide7;
+
+import com.guide.guide7.Properties.StorageProperties;
+import com.guide.guide7.Service.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService){
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
